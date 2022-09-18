08d9cb1d8ae987488593da2b5cd465cc5991a755
Tatsumi0000 <datsunote0211@gmail.com>
2022-09-18 23:35:43 +0900

    ActivityでViewModelを使うようにした

--- app/src/main/java/com/example/protodatastore_example/MainActivity.kt
+++ app/src/main/java/com/example/protodatastore_example/MainActivity.kt
@@ -2,16 +2,42 @@ package com.example.protodatastore_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: ExampleViewModel by viewModels<ExampleViewModel>()
+    private val viewModel: ExampleViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val writeButton: Button = findViewById(R.id.button1)
+        val readButton: Button = findViewById(R.id.button2)
+        val textView: TextView = findViewById(R.id.textView)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.uiState.collectLatest { uiState ->
+                textView.text = uiState.result.toString()
+            }
+        }
+
+        writeButton.setOnClickListener {
+            val randomInt = (0..100).random()
+            viewModel.write(randomInt)
+        }
+
+        readButton.setOnClickListener {
+            viewModel.read()
+        }
     }
 }
