b7058fe8bb6f79e0c70df3fb531b2b419d9b3483
Tatsumi0000 <datsunote0211@gmail.com>
2022-09-18 22:43:26 +0900

    HiltからのViewModelを使うようにした

--- app/src/main/java/com/example/protodatastore_example/MainActivity.kt
+++ app/src/main/java/com/example/protodatastore_example/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.protodatastore_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ExampleViewModel by viewModels<ExampleViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
