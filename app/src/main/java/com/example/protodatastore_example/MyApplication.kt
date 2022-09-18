d953f1831c0255dd8703545aeb7b5704a1e615c2
Tatsumi0000 <datsunote0211@gmail.com>
2022-09-18 19:11:31 +0900

    Applicationクラスを作った

---
+++ app/src/main/java/com/example/protodatastore_example/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.protodatastore_example
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+
+}
